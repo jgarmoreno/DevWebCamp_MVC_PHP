bcdeb2055ff1b5c01d13fa9d08a3fc20423d2340
Jorge García <jorgarcmoreno@gmail.com>
2024-03-09 13:07:53 +0100

    v11 | add Favicon

--- views/admin-layout.php
+++ views/admin-layout.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $titulo; ?> | DevWebCamp</title>
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;700;900&display=swap" rel="stylesheet">
