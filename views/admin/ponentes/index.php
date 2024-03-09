995001164b22250ba428d02852c5ed399df1e93c
Jorge García <jorgarcmoreno@gmail.com>
2024-03-09 10:56:42 +0100

    v3 | Ponentes

--- views/admin/ponentes/index.php
+++ views/admin/ponentes/index.php
@@ -1,1 +1,7 @@
 <h2 class="dashboard__heading"><?php echo $titulo;?></h2>
+
+<div class="dashboard__contenedor-boton">
+    <a class="dashboard__boton" href="/admin/ponentes/crear">
+        <i class="fa-solid fa-circle-plus"></i>
+        Inscribir ponente
+    </a>
